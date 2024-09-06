19c81e722fb97f56f949d155255ce107c60783f0
Adrian <adrian.vidal9589@gmail.com>
2024-09-06 16:13:46 -0600

    Configure port, add create/update timestamp annotations

--- JPANeighborhood/src/main/java/com/skilldistillery/neighbors/entities/Event.java
+++ JPANeighborhood/src/main/java/com/skilldistillery/neighbors/entities/Event.java
@@ -3,6 +3,9 @@ package com.skilldistillery.neighbors.entities;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -16,8 +19,10 @@ public class Event {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
+	@CreationTimestamp
 	@Column(name = "create_date")
 	private LocalDateTime createDate;
+	@UpdateTimestamp
 	@Column(name = "last_update")
 	private LocalDateTime lastUpdate;
 	@Column(name = "image_url")
